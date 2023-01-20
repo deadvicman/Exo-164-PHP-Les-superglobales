fbbbdb1faa242f56337b2e1ca848516bcaf6ae81
victorien <deadvicmangame@gmail.com>
2023-01-20 09:57:18 +0100

    superglobales exo 164

--- index.php
+++ index.php
@@ -7,3 +7,15 @@
  * 3. Uploadez ce projet sur votre serveur et testez !
  */
 // TODO Votre code ici
+echo "Serveur softdware: " . $_SERVER['SERVER_SOFTWARE'] . "<br>";
+echo " name: " . $_SERVER['SERVER_NAME'] . "<br>";
+echo " protocole: " . $_SERVER['SERVER_PROTOCOL'] . "<br>";
+echo " request time: " . $_SERVER['REQUEST_TIME'] . "<br>";
+echo "Serveur request methode: " . $_SERVER['REQUEST_METHOD'] . "<br>";
+echo " request URI: " . $_SERVER['REQUEST_URI'] . "<br>";
+echo "Serveur script name: " . $_SERVER['SCRIPT_NAME'] . "<br>";
+echo "Serveur remote address: " . $_SERVER['REMOTE_ADDR'] . "<br>";
+echo " remote host: " . $_SERVER['REMOTE_HOST'] . "<br>";
+echo " remote user: " . $_SERVER['REMOTE_USER'] . "<br>";
+echo " HTTP nom agent: " . $_SERVER['HTTP_USER_AGENT'] . "<br>";
+echo " HTTPS: " . $_SERVER['HTTPS'] . "<br>";
